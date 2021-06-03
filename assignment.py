49c7ba4e516f32c26d0d2c22e86591d0048b22c1
PavanCS11 <54904533+PavanCS11@users.noreply.github.com>
2021-06-03 20:06:01 +0530

    assignment.py

--- assignment.py
+++ assignment.py
@@ -5,6 +5,10 @@ Created on Thu Jun  3 19:28:19 2021
 @author: Lucifer
 """
 
+import logging
+import boto3
+from botocore.exceptions import ClientError
+
 def uniqueCode(pic_name):
     code = ''
     for character in pic_name:
@@ -12,4 +16,21 @@ def uniqueCode(pic_name):
             code = code + character
     return code
 
-uniqueCode('paul-12345.jpg')
+#uniqueCode('paul-12345.jpg')
+
+def upload_file(file_name, bucket, object_name=None):
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+    
+    file_name = uniqueCode(picture_name) + '.jpg'
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+picture_name = 'paul-12345.jpg'
